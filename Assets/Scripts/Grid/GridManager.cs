32308a1f05d1f122034c334af83484329bf1bb49
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2024-05-03 21:24:33 +0300

    fix: fix resolution of grid

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -23,33 +23,41 @@ namespace GameGuru.FirstCase.Grid
         {
             var mainCamera = Camera.main;
 
-            float cameraDistance = Mathf.Abs(mainCamera.transform.position.z); 
-            float cameraHeight = 2f * Mathf.Tan(mainCamera.fieldOfView * 0.5f * Mathf.Deg2Rad) * cameraDistance;
-            float cameraWidth = cameraHeight * mainCamera.aspect; 
+            float cameraDistance = Mathf.Abs(mainCamera.transform.position.z);
+            float cameraHeight = 2f * Mathf.Tan(mainCamera.fieldOfView * 0.5f * Mathf.Deg2Rad) * cameraDistance; 
+            float cameraWidth = cameraHeight * mainCamera.aspect;
 
-           
             float padding = cameraWidth * paddingRatio;
 
-            
-            float realGridWidth = cameraWidth - 2f * padding;
+            float leftEdge = -cameraWidth / 2f + padding;
+            float rightEdge = cameraWidth / 2f - padding;
 
-           
+            float realGridWidth = rightEdge - leftEdge;
+
             float cellSize = realGridWidth / gridSize;
 
-           
             float cellSpacing = cellSize * cellSpacingRatio;
+            cellSize -= cellSpacing;
+
+            float gridHeight = cellSize * gridSize + cellSpacing * (gridSize - 1);
+
+            if (gridHeight > cameraHeight / 2f)
+            {
+                float scaleRatio = (cameraHeight / 2f) / gridHeight;
+                cellSize *= scaleRatio;
+                cellSpacing *= scaleRatio;
+            }
 
             for (int i = 0; i < gridSize; i++)
             {
                 for (int j = 0; j < gridSize; j++)
                 {
-                   
-                    float xPos = -cameraWidth / 2f + padding + (cellSize + cellSpacing) * i + cellSize / 2f;
+                    float xPos = leftEdge + (cellSize + cellSpacing) * i + cellSize / 2f;
                     float yPos = cameraHeight / 2f - padding - (cellSize + cellSpacing) * j - cellSize / 2f;
 
-                    
                     Vector3 spawnPosition = new Vector3(xPos, yPos, 0f);
                     GameObject newCell = Instantiate(cellPrefab, spawnPosition, Quaternion.identity);
+                    newCell.transform.parent = parent;
                     newCell.transform.localScale = new Vector3(cellSize, cellSize, 1f);
                 }
             }
